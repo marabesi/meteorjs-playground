8274a20bdaec262bc698cec15cc3f6e5eb6061f4
marabesi <matheus.marabesi@gmail.com>
2017-10-06 23:14:31 -0300

    todo app with blaze template

---
+++ todo-app-blaze/imports/ui/body.js
@@ -0,0 +1,36 @@
+import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
+import './body.html';
+
+Template.body.helpers({
+    tasks() {
+        return Tasks.find({}, { sort: { createdAt: -1, done: 1 } });
+    }
+});
+
+Template.body.events({
+    'submit .new-task' (event) {
+        event.preventDefault();
+
+        const target = event.target;
+        const text = target.text.value;
+
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+        });
+
+        target.text.value = '';
+    }
+});
+
+Template.task.events({
+    'click .remove-task' (event) {
+        Tasks.remove(this._id);
+    },
+    'click .mark-as-done' (event) {
+        Tasks.update(this._id, {
+            $set: { done: ! this.done }
+        });
+    }
+});
